372963b8a7362b6c7e2ca7dd81ec534af15977af
Avani14 <trivediavani14@gmail.com>
2022-05-31 20:49:25 +0530

    Monthly wage

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,9 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Employee wage computation program on master branch!!");
 //		AbsentOrPresent attend = new AbsentOrPresent();
-		WageCalculation wc = new WageCalculation();
-		WageCalculationUsingSwitch wcs = new WageCalculationUsingSwitch();
+//		WageCalculation wc = new WageCalculation();
+//		WageCalculationUsingSwitch wcs = new WageCalculationUsingSwitch();
+		MonthlyWage mw = new MonthlyWage();
 	}
 
 }
