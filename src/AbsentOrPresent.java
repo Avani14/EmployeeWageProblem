aca438e362c203e48e6747eccf0c3f00c4670f32
Avani14 <trivediavani14@gmail.com>
2022-05-31 19:05:35 +0530

    Check if absent or present.

---
+++ src/AbsentOrPresent.java
@@ -0,0 +1,15 @@
+
+public class AbsentOrPresent {
+	static float attendance;
+	public void absentOrPresent()
+	{
+		attendance = (float) (Math.floor(Math.random()*10)%2);
+        if(attendance == 1)
+        {
+            System.out.println("Employee is present today");
+        }
+        else {
+            System.out.println("Employee is not present today");
+        }
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Employee wage computation program on master branch!!");
+		AbsentOrPresent attend = new AbsentOrPresent();
+		attend.absentOrPresent();
 	}
 
 }
